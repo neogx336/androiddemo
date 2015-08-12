6dd3a6db185eecbb1b2011de6c21953fcdd22bca
neo <neogx336@gmail.com>
2015-08-12 19:39:11 +0800

    init

---
+++ exerise01/src/com/example/exerise01/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.exerise01;
+
+import org.apache.http.client.HttpClient;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+	private Button btn;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		btn=(Button) findViewById(R.id.button1);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			//	System.out.println("33333333");
+				
+				//Toast.makeText(getApplicationContext(), "OKOK", 1000).show();
+				
+				Intent intent =new Intent(Intent.ACTION_VIEW);
+				intent.setData(Uri.parse("http://www.baidu.com"));
+				startActivity(intent);
+				
+			}
+		});
+	
+	
+		
+	}
+
+	
+}
