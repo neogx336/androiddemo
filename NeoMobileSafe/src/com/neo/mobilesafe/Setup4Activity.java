8f343d35233a73bf596381020c5af0594e46586d
neo <neogx336@gmail.com>
2015-08-14 11:32:34 +0800

    update 1

---
+++ NeoMobileSafe/src/com/neo/mobilesafe/Setup4Activity.java
@@ -0,0 +1,38 @@
+package com.neo.mobilesafe;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Setup4Activity  extends BaseSetupActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void btn_next(View view) {
+		// TODO Auto-generated method stub
+		super.btn_next(view);
+	}
+
+	@Override
+	public void btn_pre(View view) {
+		// TODO Auto-generated method stub
+		super.btn_pre(view);
+	}
+
+	@Override
+	public void showPre() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showNext() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
