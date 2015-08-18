e0d68a873ec69fd0bbaf602631205b95dbf23891
Moon-College-Android <neogx336@gmail.com>
2015-08-18 19:42:39 +0800

    在家完成DAY3内容

--- NeoMobileSafe/src/com/neo/mobilesafe/Setup4Activity.java
+++ NeoMobileSafe/src/com/neo/mobilesafe/Setup4Activity.java
@@ -1,16 +1,53 @@
 package com.neo.mobilesafe;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class Setup4Activity  extends BaseSetupActivity {
+public class Setup4Activity extends BaseSetupActivity {
+	private SharedPreferences sp;
+	private CheckBox cb_protecting;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_setup4);
+
+		sp = getSharedPreferences("config", MODE_PRIVATE);
+		cb_protecting = (CheckBox) findViewById(R.id.cb_proteting);
+		boolean protecting = sp.getBoolean("protecting", false);
+		if (protecting) {
+			cb_protecting.setText("手机防盗已经开启");
+			cb_protecting.setChecked(true);
+
+		} else {
+			cb_protecting.setChecked(false);
+			cb_protecting.setText("手机防盗已经关闭");
+		}
+		cb_protecting.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				// TODO Auto-generated method stub
+				if (isChecked) {
+					cb_protecting.setText("手机防盗已经开启");
+
+				} else {
+					cb_protecting.setText("手机防盗已经关闭");
+				}
+				Editor editor = sp.edit();
+				editor.putBoolean("protecting", isChecked);
+				editor.commit();
+			}
+		});
+
 	}
 
 	@Override
@@ -28,18 +65,24 @@ public class Setup4Activity  extends BaseSetupActivity {
 	@Override
 	public void showPre() {
 		// TODO Auto-generated method stub
-		Intent intent =new Intent (this,Setup3Activity.class);
+		Intent intent = new Intent(this, Setup3Activity.class);
 		startActivity(intent);
 		finish();
 		overridePendingTransition(R.anim.tran_pre_in, R.anim.tran_pre_out);
-		
-		
 	}
 
 	@Override
 	public void showNext() {
 		// TODO Auto-generated method stub
-		
+		Editor editor = sp.edit();
+		editor.putBoolean("configed", true);
+		editor.commit();
+
+		Intent intent = new Intent(this, LostFindActivity.class);
+		startActivity(intent);
+		finish();
+		overridePendingTransition(R.anim.tran_in, R.anim.tran_out);
+
 	}
 
 }
